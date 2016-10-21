b714b7a425e7705a41590482c4d692889f1964e2
Sindre Sorhus <sindresorhus@gmail.com>
2016-10-21 11:09:26 +0700

    init

---
+++ test.js
@@ -0,0 +1,12 @@
+import test from 'ava';
+import m from './';
+
+// See `p-map` for more comprehensive tests
+test('main', async t => {
+	t.deepEqual(await m([Promise.resolve(1), 2, 3, 4], x => x % 2), [1, 3]);
+	t.deepEqual(await m([1, 2, 3, 4], x => Promise.resolve(x % 2)), [1, 3]);
+});
+
+test('handles empty iterable', async t => {
+	t.deepEqual(await m([]), []);
+});
