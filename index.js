5fc4be644cb09cdccc7d7c2f382816f259734a83
Sindre Sorhus <sindresorhus@gmail.com>
2016-11-20 14:30:34 +0700

    remove unused argument

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 const pMap = require('p-map');
 
 module.exports = (iterable, filterer, opts) =>
-	pMap(iterable, (el, i, arr) => Promise.all([filterer(el, i, arr), el]), opts)
+	pMap(iterable, (el, i) => Promise.all([filterer(el, i), el]), opts)
 		.then(values => values.filter(x => Boolean(x[0])).map(x => x[1]));
