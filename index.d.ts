a8e7ea6ccf57bf8f576fa3caa8928475438097a3
Sindre Sorhus <sindresorhus@gmail.com>
2023-12-22 16:54:07 +0100

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -42,9 +42,9 @@ export default function pFilter<ValueType>(
 Filter promises concurrently.
 
 @param input - Iterated over concurrently in the `filterer` function.
-@param filterer - The filterer function that decides whether an element should be included into result. Expected to return `boolean | Promise<boolean>`.
+@param filterer - The filterer function that decides whether an element should be included into result.
 @param options - See the [`p-map` options](https://github.com/sindresorhus/p-map#options).
-@returns Returns an async iterable that iterates over the promises in `iterable` and ones returned from `filterer` concurrently, calling `filterer` for each element.
+@returns An async iterable that iterates over the promises in `iterable` and ones returned from `filterer` concurrently, calling `filterer` for each element.
 
 @example
 ```
@@ -72,7 +72,7 @@ const result = await pFilterIterable(places, filterer);
 console.log(result);
 //=> ['Bangkok, Thailand']
 ```
- */
+*/
 export function pFilterIterable<ValueType>(
 	input: Iterable<ValueType | PromiseLike<ValueType>>,
 	filterer: (
